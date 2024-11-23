e5e94224589044a29c52626840c7f6c12530cb0a
Chase Little <chaselittle@Chases-MacBook-Pro-2.local>
2024-11-23 01:32:09 -0800

    add create platforms to gamescene

--- src/scripts/game/GameScene.ts
+++ src/scripts/game/GameScene.ts
@@ -1,25 +1,56 @@
 import Game from "../Game";
-import Scene from "../system/Scene";
 import { Background } from "./Background";
 import { Hero } from "./Hero";
+import * as Matter from "matter-js";
+import Platforms from "./Platforms";
+import { Container } from "pixi.js";
 
-export default class GameScene extends Scene {
+export default class GameScene {
+  public game: Game;
   public bg!: Background;
   public hero!: Hero;
+  public platforms!: Platforms;
+  public container: Container;
 
   constructor(game: Game) {
-    super(game);
     this.game = game;
+    this.container = new Container();
     this.container.interactive = true;
+    this.create();
+    this.update = this.update.bind(this);
+    this.game.app.ticker.add(this.update, this);
   }
   async create(): Promise<void> {
     this.createBackground();
     await this.createHero();
+    this.createPlatforms(this.game);
+  }
+
+  onCollisionStart(event: any) {
+    const colliders = [event.pairs[0].bodyA, event.pairs[0].bodyB];
+    const hero = colliders.find((body) => body.gameHero);
+    const platform = colliders.find((body) => body.gamePlatform);
+    // const powerUp = colliders.find(body => body.gamePowerUp);
+
+    if (hero && platform) {
+      this.hero.stayOnPlatform(platform.gamePlatform);
+    }
+
+    // const diamond = colliders.find(body => body.gameDiamond);
+
+    // if (hero && diamond) {
+    //     this.hero.collectDiamond(diamond.gameDiamond);
+    // }
+
+    // if (hero && powerUp){
+    //     this.hero.activatePowerUp(powerUp.gamePowerUp);
+    // }
   }
 
   createBackground(): void {
-    this.bg = new Background(this.game);
-    this.container.addChild(this.bg.container);
+    const bg = new Background(this.game);
+    this.container.addChild(bg.container);
+    this.bg = bg;
   }
 
   async createHero(): Promise<void> {
@@ -33,5 +64,37 @@ export default class GameScene extends Scene {
     this.container.on("pointerdown", () => {
       this.hero.startJump();
     });
+  }
+
+  createPlatforms(game: Game) {
+    const platforms = new Platforms(game);
+
+    this.container.addChild(platforms.container);
+    this.platforms = platforms;
+    console.log(this.platforms)
+  }
+
+  update(dt: any) {
+    this.bg.update(dt);
+    if (this.platforms) {
+      this.platforms.update();
+    }
+  }
+
+  destroy() {
+    console.log("a");
+    Matter.Events.off(
+      this.game.physics,
+      "collisionStart",
+      this.onCollisionStart.bind(this)
+    );
+    this.game.app.ticker.remove(this.update, this);
+    if (this.bg && this.hero) {
+      this.bg.destroy();
+      this.hero.destroy();
+    }
+    // this.platforms.destroy();
+    // this.labelScore.destroy();
+    // App.diamondManager.destroy();
   }
 }
